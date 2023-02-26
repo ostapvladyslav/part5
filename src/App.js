efba597f73114a3a081a16906c19aca13340d23d
Vladyslav Ostapchuk <ostapvladyslav@gmail.com>
2023-02-26 21:47:44 +0200

    Exercise 5.1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,78 @@
 import { useState, useEffect } from 'react';
 import Blog from './components/Blog';
 import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
-  return (
+  const loginForm = () => (
+    <>
+      <h2>log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type='text'
+            value={username}
+            name='Username'
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+
+        <div>
+          password
+          <input
+            type='password'
+            value={password}
+            name='Password'
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+
+        <button type='submit'>login</button>
+      </form>
+    </>
+  );
+
+  const blogForm = () => (
     <div>
       <h2>blogs</h2>
+
+      <p>{user.name} logged in</p>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
+    </div>
+  );
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      console.log('Wrong credentials');
+    }
+  };
+
+  return (
+    <div>
+      <>{user === null ? loginForm() : blogForm()}</>
     </div>
   );
 };
