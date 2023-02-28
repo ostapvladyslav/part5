1de58e152115e922a4ced21d070cacde294338d7
Vladyslav Ostapchuk <ostapvladyslav@gmail.com>
2023-02-28 02:31:15 +0200

    Exercise 5.10

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -3,9 +3,11 @@ import Blog from './Blog';
 const BlogList = ({ blogs, user, updateBlog }) => {
   return (
     <>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} user={user} updateBlog={updateBlog} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} user={user} updateBlog={updateBlog} />
+        ))}
     </>
   );
 };
